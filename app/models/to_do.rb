3dffa63e90baa5e50e00183129f02b59210a1600
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-06-03 09:38:51 -0300

    Add Rubocop changes and add "has many :to_dos" to UserTask

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Element of user list
 class ToDo < ApplicationRecord
   belongs_to :user_task, optional: true
 end
--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+# model that replicates some to_dos
 class UserTask < ApplicationRecord
+  has_many :to_do
 end
