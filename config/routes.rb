90c407324ccaabeea53513c495fc21fb939b0c0f
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-06-18 09:23:29 -0300

    Add root path as to_dos#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :to_dos
   resources :user_tasks
+  root "to_dos#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
