f3d42e33386922a453c2000fe8aa1f83f95b8e08
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-06-18 09:32:19 -0300

    Add Devise authentication to user_tasks Controller

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -1,4 +1,5 @@
 class UserTasksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_user_task, only: %i[ show edit update destroy ]
 
   # GET /user_tasks or /user_tasks.json
