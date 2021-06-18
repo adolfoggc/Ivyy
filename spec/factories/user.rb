4ba1e7c6ecc0761b80f2ba25349ee21c1e93ba68
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-06-18 09:20:41 -0300

    Install rspec

---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# User factory
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    after(:build) { |u| u.password_confirmation = u.password = Faker::Internet.password }
+  end
+end
